c56e18c6b64619f592ce968ccdcffdaf2506fd80
jaracar84 <cajfdeveloper@gmail.com>
2024-05-21 15:46:28 -0500

    todoReducer agregado

--- useTodo/useTodo.js
+++ useTodo/useTodo.js
@@ -1,5 +1,5 @@
 import { useEffect, useReducer } from 'react';
-import { todoReducer } from '../08-useReducer/todoReducer';
+import { todoReducer } from './todoReducer';
 
 const initialState = [];
 
@@ -34,9 +34,8 @@ export const useTodo = () => {
     dispatch({
       type: 'toggle Todo',
       payload: id,
-
-    })
-  }
+    });
+  };
 
   return {
     todos,
